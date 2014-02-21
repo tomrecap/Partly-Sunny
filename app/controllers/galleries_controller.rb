00e74e82ea2d40fab82b76fc4bc5099a7b4da0f4
App Academy Student <student@appacademy.io>
2014-02-21 10:07:25 -0500

    Adds kaminari methods to galleries#show

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -2,29 +2,22 @@ class GalleriesController < ApplicationController
   before_filter :require_signed_in!
 
   def show
-    starting_photo = (params[:offset] || 0)
-
     @cities = City.all
 
-    # make this less repetitive
+    # use includes(:photos)?
     if params[:city_id]
       @subject = City.find(params[:city_id])
-      @photos = @subject.photos
-        .limit(10).offset(starting_photo).order("created_at DESC")
       @name = @subject.name
     elsif params[:user_id]
       @subject = User.find(params[:user_id])
-      @photos = @subject.photos
-        .limit(10).offset(starting_photo).order("created_at DESC")
       @name = @subject.user_name
     elsif params[:tag_id]
       @subject = Tag.find(params[:tag_id])
-      @photos = @subject.photos
-        .limit(10).offset(starting_photo).order("created_at DESC")
       @name = @subject.body
     end
 
-
+    @photos = @subject.photos
+      .page(params[:page]).per(10).order("created_at DESC")
   end
 
 end
