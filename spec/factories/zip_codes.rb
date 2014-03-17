39305325e2d9f8f2ae4142ecaf17afe95c1693e1
Thomas Carroll <thomas.carroll08@gmail.com>
2014-03-17 10:54:40 -0400

    Adds zip code specs & changes database usernames

--- spec/factories/zip_codes.rb
+++ spec/factories/zip_codes.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :zip_code do
-    zip_code { |z| "99999"}
-    city { |c| "City"}
+    zip_code { |z| rand(10000..99999).to_s }
+    city { |c| "City" }
     state_name { |s| "State" }
     state_code { |s| "ST" }
     longitude { |l| -100 }
--- spec/models/zip_code_spec.rb
+++ spec/models/zip_code_spec.rb
@@ -11,6 +11,42 @@ describe ZipCode do
   it { should validate_presence_of(:longitude) }
   it { should validate_presence_of(:latitude) }
 
-  it { should validate_uniqueness_of(:zip_code) }
-
+  it do
+    FactoryGirl.create(:zip_code)
+    should validate_uniqueness_of(:zip_code)
+  end
+  
+  context "nearby_zip_codes" do
+    zip_codes = [
+      "11110",
+      "11111",
+      "11112",
+      "11113",
+      "11114",
+      "11115"]
+    coords = [100, 100]
+    zip_code_objects = []
+    
+    zip_codes.each do |zip_code_value|
+      zip_code_objects << FactoryGirl.create(
+        :zip_code,
+        zip_code: zip_code_value,
+        latitude: coords.first,
+        longitude: coords.last
+      )
+      coords.map! { |coord| coord + 0.09 }      
+    end
+    
+    it "should find zip codes within 0.2 degrees of lat & long" do
+      nearby_zip_code_objects = zip_code_objects
+        .first.nearby_zip_codes
+      nearby_zip_codes = nearby_zip_code_objects.map do |z_c|
+        z_c.zip_code
+      end
+      
+      expect(nearby_zip_codes).to include("11110")
+      expect(nearby_zip_codes).to include("11111", "11112")
+      expect(nearby_zip_codes).to_not include("11113", "11114", "11115")
+    end
+  end
 end
